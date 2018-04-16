92d171a8fbbf559e59de810f59639d4cdfaeb0a7
moralessebastian46 <moralessebastian46@gmail.com>
2018-04-16 06:15:06 -0300

    Primer aproximacion de la Instancia

--- Instancia/src/Instancia.c
+++ Instancia/src/Instancia.c
@@ -8,10 +8,45 @@
  ============================================================================
  */
 
-#include <stdio.h>
+/*#include <stdio.h>
 #include <stdlib.h>
 
 int main(void) {
-	puts(""); /* prints  */
+	puts(""); // prints
 	return EXIT_SUCCESS;
 }
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+const char* HOST_NAME = "127.0.0.1";
+const int PORT = 8080;
+
+int main(void) {
+	struct sockaddr_in direccionServidor;
+	direccionServidor.sin_family = AF_INET;
+	direccionServidor.sin_addr.s_addr = inet_addr(HOST_NAME);
+	printf("%s", HOST_NAME);
+	direccionServidor.sin_port = htons(PORT);
+
+	int cliente = socket(AF_INET, SOCK_STREAM, 0);
+	if (connect(cliente, (void*) &direccionServidor, sizeof(direccionServidor)) != 0) {
+		perror("No se pudo conectar al Coordinador");
+		return 1;
+	}
+
+	// Se envia mensaje al coordinador
+	while (1) {
+		char mensaje[1000];
+		scanf("%s", mensaje);
+
+		send(cliente, mensaje, strlen(mensaje), 0);
+	}
+
+	return 0;
+}
+
