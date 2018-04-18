3572ee5eea5a53474fd6df58429da69168deb551
Lautaro Haase <lautarohaase_23@hotmail.com>
2018-04-17 21:55:26 -0300

    primeras líneas de ESI

--- ESI/esi.c
+++ ESI/esi.c
@@ -10,4 +10,31 @@
 
 int main() {
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+int main(void) {
+	struct sockaddr_in direccionServidor;
+	direccionServidor.sin_family = AF_INET;
+	direccionServidor.sin_addr.s_addr = inet_addr(IP);
+	direccionServidor.sin_port = htons(PUERTO);
+
+	Readline("asdf");
+	int cliente = socket(AF_INET, SOCK_STREAM, 0);
+	if (connect(cliente, (void*) &direccionServidor, sizeof(direccionServidor)) != 0) {
+		perror("No se pudo conectar al Coordinador");
+		return 1;
+	}
+
+	while (1) {
+		char * mensaje = Readline("Ingrese mensaje:");
+
+		send(cliente, &mensaje, strlen(mensaje), 0);
+	}
+
+	return 0;
+}
 }
