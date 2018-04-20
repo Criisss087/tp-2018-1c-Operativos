aaa32bcec5cf19339c4f13dbe4a121d56f339a87
Lautaro Haase <lautarohaase_23@hotmail.com>
2018-04-19 21:08:33 -0300

    reordenacion de proyectos

---
+++ ESI/src/ESI.c
@@ -0,0 +1,16 @@
+/*
+ ============================================================================
+ Name        : ESI.c
+ Author      : La Orden Del Socket
+ Version     :
+ Copyright   : Si nos copias nos desaprueban el coloquio
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts(""); /* prints  */
+	return EXIT_SUCCESS;
+}
---
+++ Instancia/src/Instancia.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Instancia.c
+ Author      : La Orden Del Socket
+ Version     :
+ Copyright   : Si nos copias nos desaprueban el coloquio
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts(""); /* prints  */
+	return EXIT_SUCCESS;
+}
