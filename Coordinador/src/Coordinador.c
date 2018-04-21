60d2fe178188219992a8e353f9cee96318a186e0
utnso <tamara.m94@hotmail.com>
2018-04-21 13:04:09 -0300

    coordinador sin conexion

--- Coordinador/src/Coordinador.c
+++ Coordinador/src/Coordinador.c
@@ -9,10 +9,69 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <signal.h>
+#define MIPUERTO 5050    // Puerto al que conectarán los usuarios
+#define BACKLOG 3     // Cuántas conexiones pendientes se mantienen en cola
 
-int main(void) {
-	puts(""); /* prints  */
+int main(void)
+{
+    int sockfd, new_fd;  // Escuchar sobre sock_fd, nuevas conexiones sobre new_fd
+    struct sockaddr_in my_addr;    // información sobre mi dirección
+    struct sockaddr_in their_addr; // información sobre la dirección del cliente
+    int sin_size;
+    int yes=1;
+
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+            perror("socket");
+            exit(1);
+        }
+
+        /*if (setsockopt(sockfd,SOL_SOCKET,SO_REUSEADDR,&yes,sizeof(int)) == -1) {
+            perror("setsockopt");
+            exit(1);
+        }*/ //para liberar un puerto que estuvo usado?)
+
+        my_addr.sin_family = AF_INET;         // Ordenación de bytes de la máquina
+        my_addr.sin_port = htons(MIPUERTO);     // short, Ordenación de bytes de la red
+        my_addr.sin_addr.s_addr = INADDR_ANY; // Rellenar con mi dirección IP
+        memset(&(my_addr.sin_zero), '\0', 8); // Poner a cero el resto de la estructura
+
+        if (bind(sockfd, (struct sockaddr *)&my_addr, sizeof(struct sockaddr))== -1) {
+            perror("bind");
+            exit(1);
+        }
+
+        if (listen(sockfd, BACKLOG) == -1) {
+            perror("listen");
+            exit(1);
+        }
+
+        while(1) {  // main accept() loop
+            sin_size = sizeof(struct sockaddr_in);
+            if ((new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size)) == -1) {
+                perror("accept");
+                continue;
+            }
+            printf("server: got connection from %s\n", inet_ntoa(their_addr.sin_addr));
+            if (!fork()) { // Este es el proceso hijo
+                close(sockfd); // El hijo no necesita este descriptor
+                if (send(new_fd, "Hello, world!\n", 14, 0) == -1)
+                    perror("send");
+                close(new_fd);
+                exit(0);
+            }
+            close(new_fd);  // El proceso padre no lo necesita
+        }
+
+        return 0;
+    }
 	return EXIT_SUCCESS;
 }
 
