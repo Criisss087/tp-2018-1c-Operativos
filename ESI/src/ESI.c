7dfb1bdd4c959d31c0c91a91df4c2057ae2e5b37
tmerchan <tamara.m94@hotmail.com>
2018-05-01 22:47:30 -0300

    implementacion del parser y lectura de un archivo

--- ESI/src/ESI.c
+++ ESI/src/ESI.c
@@ -14,12 +14,17 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <unistd.h>
+#include <parsi/parser.h>
 
 #define IP "127.0.0.1"
 #define PUERTO "8080"
 #define PACKAGESIZE 1024	// Define cual va a ser el size maximo del paquete a enviar
 
-int main(){
+int main(int argc, char **argv){
+	FILE * fp;
+	char * line = NULL;
+	size_t len = 0;
+	ssize_t read;
 
 	struct addrinfo hints;
 	struct addrinfo *serverInfo;
@@ -47,6 +52,42 @@ int main(){
 
 //	while(enviar){
 	while(1){
+
+		fp = fopen(argv[1], "r");
+		if (fp == NULL){
+		    perror("Error al abrir el archivo: ");
+		    // informar a quien corresponda -- exit(EXIT_FAILURE);
+		}
+
+		while ((read = getline(&line, &len, fp)) != -1) {
+		     t_esi_operacion parsed = parse(line);
+			        if(parsed.valido){
+			            switch(parsed.keyword){
+			                case GET:
+			                    printf("GET\tclave: <%s>\n", parsed.argumentos.GET.clave);
+			                    break;
+			                case SET:
+			                    printf("SET\tclave: <%s>\tvalor: <%s>\n", parsed.argumentos.SET.clave, parsed.argumentos.SET.valor);
+			                    break;
+			                case STORE:
+			                    printf("STORE\tclave: <%s>\n", parsed.argumentos.STORE.clave);
+			                      break;
+			                default:
+			                    fprintf(stderr, "No pude interpretar <%s>\n", line);
+			                    // informar a quien corresponda -- exit(EXIT_FAILURE);
+			            }
+
+			        destruir_operacion(parsed);
+			        } else {
+			            fprintf(stderr, "La linea <%s> no es valida\n", line);
+			            // informar a quien corresponda -- exit(EXIT_FAILURE);
+			        }
+			    }
+
+			    fclose(fp);
+			    if (line)
+			        free(line);
+
 		// Lee una linea en el stdin (lo que escribimos en la consola) hasta encontrar un \n (y lo incluye) o llegar a PACKAGESIZE.
 		fgets(message, PACKAGESIZE, stdin);/*
 			// Chequeo que el usuario no quiera salir
@@ -59,3 +100,4 @@ int main(){
 	close(serverSocket);
 		return 0;
 }
+
