a85c9dfa5a2a4480502711d5c6528c31488c3595
tmerchan <tamara.m94@hotmail.com>
2018-05-04 19:47:22 -0300

    el ESI puede parsear y enviar la info al coord y al planif, falta probar

--- ESI/src/ESI.c
+++ ESI/src/ESI.c
@@ -15,17 +15,13 @@
 #include <netdb.h>
 #include <unistd.h>
 #include <parsi/parser.h>
+#define PACKAGESIZE 1024
+#define RESPUESTA 1024
 
-#define IP "127.0.0.1"
-#define PUERTO "8080"
-#define PACKAGESIZE 1024	// Define cual va a ser el size maximo del paquete a enviar
+/*#define IP "127.0.0.1"
+#define PUERTO "8080"*/ //LO comento porque necesito que se conecte a 2 servidores, no solo el coordinador
 
-int main(int argc, char **argv){
-	FILE * fp;
-	char * line = NULL;
-	size_t len = 0;
-	ssize_t read;
-
+struct addrinfo* crear_addrinfo(){
 	struct addrinfo hints;
 	struct addrinfo *serverInfo;
 
@@ -35,69 +31,96 @@ int main(int argc, char **argv){
 
 	getaddrinfo(IP, PUERTO, &hints, &serverInfo);
 
-	int serverSocket;
-	serverSocket = socket(serverInfo->ai_family, serverInfo->ai_socktype, serverInfo->ai_protocol);
+	return serverInfo;
+}
 
-	// Las siguientes dos lineas sirven para no lockear el address
+int main(int argc, char **argv){
+	FILE * fp;
+	char * line = NULL;
+	size_t len = 0;
+	ssize_t read;
+
+	struct addrinfo *serverInfo1 = crear_addrinfo();
+	struct addrinfo *serverInfo2 = crear_addrinfo();
+
+	int serverSocket1 = socket(serverInfo1->ai_family, serverInfo1->ai_socktype, serverInfo1->ai_protocol); //coordinador
+	int serverSocket2 = socket(serverInfo2->ai_family, serverInfo2->ai_socktype, serverInfo2->ai_protocol); //planificador
+
+
+	/* Las siguientes dos lineas sirven para no lockear el address
 		int activado = 1;
-		setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &activado, sizeof(activado));
+		setsockopt(serverSocket1, SOL_SOCKET, SO_REUSEADDR, &activado, sizeof(activado));*/
 
-	int con = connect(serverSocket, serverInfo->ai_addr, serverInfo->ai_addrlen);
-	freeaddrinfo(serverInfo);
+	int coord = connect(serverSocket1, serverInfo1->ai_addr, serverInfo1->ai_addrlen);
+	int planif = connect(serverSocket2, serverInfo2->ai_addr, serverInfo2->ai_addrlen);
+
+	freeaddrinfo(serverInfo1);
+	freeaddrinfo(serverInfo2);
 
 	int enviar = 1;
-	char message[PACKAGESIZE];
 
-	printf("Conectado al servidor: %d \n",con);
+	printf("Conectado al servidor: %d \n",coord);
+	printf("Conectado al servidor: %d \n",planif);
+
+	char message1[] = "Error al abrir el archivo";
 
 //	while(enviar){
 	while(1){
 
 		fp = fopen(argv[1], "r");
 		if (fp == NULL){
-		    perror("Error al abrir el archivo: ");
-		    // informar a quien corresponda -- exit(EXIT_FAILURE);
+		    send(serverSocket2, message1, strlen(message1)+1, 0);// informar al planificador -- exit(EXIT_FAILURE);
 		}
 
-		while ((read = getline(&line, &len, fp)) != -1) {
-		     t_esi_operacion parsed = parse(line);
+		char package[PACKAGESIZE];
+		char respuestaDelGet[RESPUESTA];
+		char respuestaDelSet[RESPUESTA];
+		char respuestaDelStore[RESPUESTA];
+
+		int status = 1;
+
+		while (status != 0){
+		    	status = recv(serverSocket2, (void*) package, PACKAGESIZE, 0);//el planificador envia la orden(package)
+		    	if (status != 0) {
+		    		if ((read = getline(&line, &len, fp)) != -1) {
+		            t_esi_operacion parsed = parse(line);
 			        if(parsed.valido){
 			            switch(parsed.keyword){
 			                case GET:
-			                    printf("GET\tclave: <%s>\n", parsed.argumentos.GET.clave);
+			                    send(serverSocket1,parsed.argumentos.GET.clave, strlen(parsed.argumentos.GET.clave)+1,0); //le envia al coordinador la clave
+			                    recv(serverSocket1, respuestaDelGet, RESPUESTA,0);//espera la rta del coordinador
+			                    send(serverSocket2, respuestaDelGet, RESPUESTA,0);//cuando le llega la respuesta se la envia al planificador
 			                    break;
 			                case SET:
-			                    printf("SET\tclave: <%s>\tvalor: <%s>\n", parsed.argumentos.SET.clave, parsed.argumentos.SET.valor);
+			                	send(serverSocket1,parsed.argumentos.SET.clave, strlen(parsed.argumentos.SET.clave)+1,0);//idem
+			                	send(serverSocket1,parsed.argumentos.SET.valor, strlen(parsed.argumentos.SET.valor)+1,0);
+			                	recv(serverSocket1, respuestaDelSet, RESPUESTA,0);//espera la rta del coordinador
+			                    send(serverSocket2, respuestaDelSet, RESPUESTA,0);//le envia la rta del coordinador al planificador...
 			                    break;
 			                case STORE:
-			                    printf("STORE\tclave: <%s>\n", parsed.argumentos.STORE.clave);
-			                      break;
+			                	send(serverSocket1,parsed.argumentos.STORE.clave, strlen(parsed.argumentos.STORE.clave)+1,0);
+			                	recv(serverSocket1, respuestaDelStore, RESPUESTA,0);
+			                	send(serverSocket2, respuestaDelStore, RESPUESTA,0);
+			                    break;
 			                default:
-			                    fprintf(stderr, "No pude interpretar <%s>\n", line);
-			                    // informar a quien corresponda -- exit(EXIT_FAILURE);
+			                	send(serverSocket2, message2, strlen(message2)+1, 0);// informar al planificador
 			            }
-
-			        destruir_operacion(parsed);
-			        } else {
-			            fprintf(stderr, "La linea <%s> no es valida\n", line);
-			            // informar a quien corresponda -- exit(EXIT_FAILURE);
+			            destruir_operacion(parsed);
 			        }
+		    	    }
 			    }
+		}
 
 			    fclose(fp);
 			    if (line)
 			        free(line);
 
-		// Lee una linea en el stdin (lo que escribimos en la consola) hasta encontrar un \n (y lo incluye) o llegar a PACKAGESIZE.
-		fgets(message, PACKAGESIZE, stdin);/*
-			// Chequeo que el usuario no quiera salir
-			if (!strcmp(message,"exit\n")) enviar = 0;
-			// Solo envio si el usuario no quiere salir.*/
-			//if (enviar)
-				send(serverSocket, message, strlen(message)+1, 0);
 		}
 
-	close(serverSocket);
+
+
+	close(serverSocket1);
+	close(serverSocket2);
 		return 0;
 }
 
