ac1f62e822bf3aada24b29ecd515b04c0dc9b1bb
Cristian Barrientos <cristian.barrientos@hotmail.com>
2018-05-13 23:40:01 -0300

    Select implementado, servidor de esis, trata varios esis a la ves, y maneja la consola. Queda pendiente el cliente hacia el coordinador, y las exepciones del select. Sin memory leaks

--- Planificador/src/Planificador.c
+++ Planificador/src/Planificador.c
@@ -12,213 +12,240 @@
 
 int main(void) {
 
-	pthread_t t_cord_id;
-	pthread_t t_servidor_id;
-	pthread_t t_consola_id;
+	fd_set readset, writeset, exepset;
+	int max_fd;
+	char read_buffer[MAX_LINEA];
 
-	pthread_attr_t t_cord_attr;
-	pthread_attr_t t_servidor_attr;
-	pthread_attr_t t_consola_attr;
+	//Creo el socket servidor para recibir ESIs (ya bindeado y escuchando)
+	int serv_socket = iniciar_servidor(PORT_ESCUCHA);
 
-	// Conectar al Coordinador
-	pthread_attr_init(&t_cord_attr);
-	pthread_create(&t_cord_id, &t_cord_attr, conectar_coordinador, NULL);
-	pthread_join(t_cord_id, NULL);
+	//Creo el socket cliente para conectarse al coordinador
+	// int coord_socket = conectar_coordinador(IP_COORD, PORT_COORD );
 
+	inicializar_conexiones_esi();
+	stdin_no_bloqueante();
+
+	while(TRUE){
+		//Inicializa los file descriptor
+		FD_ZERO(&readset);
+		FD_ZERO(&writeset);
+		FD_ZERO(&exepset);
+
+		//Agrega el fd del socket servidor al set de lectura y excepciones
+		FD_SET(serv_socket, &readset);
+		FD_SET(serv_socket, &exepset);
+
+		//Agrega el fd del socket coordinador al set de lectura
+		//FD_SET(coord_socket, &readset);
+		//FD_SET(coord_socket, &exepset);
+
+		//Agrega el stdin para leer la consola
+		FD_SET(STDIN_FILENO, &readset);
+		//FD_SET(fileno(stdin), &exepset);
+
+		/* Seteo el maximo file descriptor necesario para el select */
+		max_fd = serv_socket;
+
+		//Agrega los conexiones esi existentes
+		for (int i = 0; i < MAX_CLIENTES; i++)
+		{
+			if (conexiones_esi[i].socket != NO_SOCKET)
+			{
+				FD_SET(conexiones_esi[i].socket, &readset);
+				FD_SET(conexiones_esi[i].socket, &writeset);
+				FD_SET(conexiones_esi[i].socket, &exepset);
+			}
+
+			if (conexiones_esi[i].socket > max_fd)
+				max_fd = conexiones_esi[i].socket;
+
+		}
+
+		/* if(max_fd < coord_socket)
+			max_fd = coord_socket;
+		*/
+
+		int result = select(max_fd+1, &readset, &writeset, &exepset, NULL); //&time);
+
+		if(result == 0)
+			printf("Select time out\n");
+		else if(result < 0)
+			printf("Error en select\n");
+		else if(result > 0) //Hubo un cambio en algun fd
+		{
+
+			//Aceptar nuevas conexiones de ESI
+			if (FD_ISSET(serv_socket, &readset)) {
+				int nuevo_esi = atender_nuevo_esi(serv_socket);
+
+			}
 /*
-	// Iniciarse como servidor
-	pthread_attr_init(&t_servidor_attr);
-	pthread_create(&t_servidor_id, &t_servidor_attr, (void *)iniciar_servidor, NULL);
-	pthread_join(t_servidor_id, NULL);
+			//Atender al coordinador
+			if(FD_ISSET(coord_socket, &readset))
+			{
+
+			}
 */
+			//Se ingresó algo a la consola
+			if(FD_ISSET(STDIN_FILENO, &readset))
+			{
 
-	// Abrir la consola
-	pthread_attr_init(&t_consola_attr);
-	pthread_create(&t_consola_id, &t_consola_attr, (void *)consola, NULL);
-	pthread_join(t_consola_id, NULL);
+				read_from_stdin(read_buffer, MAX_LINEA);
 
+				int res = comando_consola(read_buffer);
+				if(res)
+					break;
+
+			}
+
+			//Manejo de conexiones esi ya existentes
+			for (int i = 0; i < MAX_CLIENTES; ++i) {
+				if (conexiones_esi[i].socket != NO_SOCKET ){
+					//Mensajes nuevos de algun esi
+					if (FD_ISSET(conexiones_esi[i].socket, &readset)) {
+						if(recibir_mensaje_esi(conexiones_esi[i].socket) == 0)
+						{
+							cerrar_conexion_esi(&conexiones_esi[i]);
+							continue;
+						}
+
+					//Excepciones del esi, para la desconexion
+						if (FD_ISSET(conexiones_esi[i].socket, &exepset)) {
+							if(recibir_mensaje_esi(conexiones_esi[i].socket) == 0)
+							{
+								cerrar_conexion_esi(&conexiones_esi[i]);
+								continue;
+							}
+						}
+					} //if isset
+				} // if NO_SOCKET
+			} //for conexiones_esi
+		} //if result select
+	} //while
 
 	return EXIT_SUCCESS;
 }
 
-void *conectar_coordinador() {
+int conectar_coordinador(char * ip, char * port) {
 
-	int server_socket;
 	int familiaProt = PF_INET;
 	int tipo_socket = SOCK_STREAM;
 	int protocolo = 0; //ROTO_TCP;
 
 	struct addrinfo hints;
-	struct addrinfo *server_info;
+	struct addrinfo *coord_info;
 
 	memset(&hints, 0, sizeof(hints));
 	hints.ai_family = AF_UNSPEC; // Permite que la maquina se encargue de verificar si usamos IPv4 o IPv6
 	hints.ai_socktype = SOCK_STREAM;  // Indica que usaremos el protocolo TCP
 
-	getaddrinfo(IP, PORT, &hints, &server_info); // Carga en server_info los datos de la conexion
+	getaddrinfo(ip, port, &hints, &coord_info); // Carga en server_info los datos de la conexion
 
 
 	printf("\nIniciando como cliente hacia el coordinador...\n");
 
-	server_socket = socket(server_info->ai_family, server_info->ai_socktype, server_info->ai_protocol);
-	if (server_socket != -1)
+	int coord_socket = socket(coord_info->ai_family, coord_info->ai_socktype, coord_info->ai_protocol);
+	if (coord_socket != -1)
 		printf("Socket creado correctamente!\n");
 	else
 		printf("Error al crear el socket\n");
 
 	int activado = 1;
-	setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &activado, sizeof(activado));
+	setsockopt(coord_socket, SOL_SOCKET, SO_REUSEADDR, &activado, sizeof(activado));
 
-
-	int res_connect = connect(server_socket, server_info->ai_addr, server_info->ai_addrlen);
+	//TODO probar accept en un servidor dummy
+/*	int res_connect = connect(coord_socket, coord_info->ai_addr, coord_info->ai_addrlen);
 	if (res_connect < 0)
 	{
-		printf("Error al intentar conectar al servidor\n");
+		printf("Error al intentar conectar al coordinador\n");
+		close(coord_socket);
 		pthread_exit(0);
 	}
 	else
-		printf("Conectando con el servidor...\n");
+		printf("Conectando con el coordinador...\n");
 
-	freeaddrinfo(server_info);
-
-	char *mensaje = malloc(28);
-	strcpy(mensaje,"Se conectó el planificador\n");
-
-	//Intento enviar mensaje al coordinador
-	int res_send = send(server_socket, mensaje, sizeof(mensaje), 0);
-	printf("Intentando mandar un mensaje...\n");
-
-	free(mensaje);
-
-	int res_close = close(server_socket);
-	printf("Cerrando el socket y saliendo el hilo...\n");
+	freeaddrinfo(coord_info);
+*/
+	return coord_socket;
 
 
-	pthread_exit(0);
 }
 
 
-void *iniciar_servidor() {
+int iniciar_servidor(unsigned short port)
+{
 
-	struct sockaddr_in dir_serv_pl;
-	struct sockaddr_in dir_cli_pl;
+	struct sockaddr_in dir_serv_sock;
 
 	unsigned int dir_cli_size;
 
-	dir_serv_pl.sin_family = AF_INET;
-	dir_serv_pl.sin_addr.s_addr = INADDR_ANY;
-	dir_serv_pl.sin_port = htons(8080);
+	dir_serv_sock.sin_family = AF_INET;
+	dir_serv_sock.sin_addr.s_addr = INADDR_ANY;
+	dir_serv_sock.sin_port = htons(port);
 
-	printf("Iniciando como servidor...\n");
+	printf("Creando socket servidor...\n");
 
-	int serv_pl = socket(AF_INET, SOCK_STREAM, 0);
+	int server_socket = socket(AF_INET, SOCK_STREAM, 0);
 
 	int activado = 1;
-	setsockopt(serv_pl,SOL_SOCKET,SO_REUSEADDR, &activado, sizeof(activado));
+	setsockopt(server_socket,SOL_SOCKET,SO_REUSEADDR, &activado, sizeof(activado));
 
 
-	if(bind(serv_pl, (void*)&dir_serv_pl, sizeof(dir_serv_pl)) != 0)
+	if(bind(server_socket, (void*)&dir_serv_sock, sizeof(dir_serv_sock)) != 0)
 	{
-		printf("Falló el bind del servidor\n");
+		printf("Falló el bind del socket servidor\n");
 		exit(1);
 	}
 
 
-	printf("Servidor escuchando\n");
-	listen(serv_pl, 10);
+	printf("Socket servidor (%d) escuchando\n", server_socket);
+	listen(server_socket, 10);
+
+	return server_socket;
+
+	/*
+
+	struct sockaddr_in dir_cli_pl;
 
 	int cliente = accept(serv_pl, (void*)&dir_cli_pl, &dir_cli_size);
 	printf("Recibí una nueva coneccion (%d) \n", cliente);
 	send(cliente, "Te conectaste al planificador!\n",32,0);
 
-	for(;;);
+	//for(;;);
 
 
-	//close(serv_pl);
+	close(serv_pl);
 	pthread_exit(0);
+*/
 
 }
 
-
+/*	Consola preparada para funcionar en un hilo aparte
+ * 	actualmente no se está usando ya que readline es una funcion bloqueante
+ * 	y necesito que el select pueda leer de stdin mientras maneja las conexiones
+ * 	Es código duplicado, pero la dejo porque está bonita :)
+ */
 
 void *consola() {
 
-	int comando_key;
+	int res=0;
 	char *buffer = NULL;
-	char *comando = NULL;
-	char *parametro1 = NULL;
-	char *parametro2 = NULL;
 
 	printf("\nAbriendo consola...\n");
 
-	while(1){
+	while(TRUE){
 
 		//Trae la linea de consola
 		buffer = readline(">");
 
-		// Separa la linea de consola en comando y sus parametros
-		obtener_parametros(buffer, &comando, &parametro1, &parametro2);
-		//printf("comando: %s p1: %s p2: %s\n",comando,parametro1,parametro2);
+		res = comando_consola(buffer);
+
 		free(buffer);
 
-		// Obtiene la clave del comando a ejecutar para el switch
-		comando_key = obtener_key_comando(comando);
-
-		switch(comando_key){
-			case pausar:
-				pausar_consola();
-				break;
-			case continuar:
-				continuar_consola();
-				break;
-			case bloquear:
-				bloquear_clave(parametro1,parametro2);
-				break;
-			case desbloquear:
-				desbloquear_clave(parametro1, parametro2);
-				break;
-			case listar:
-				listar_recurso(parametro1);
-				break;
-			case kill:
-				kill_id(parametro1);
-				break;
-			case status:
-				status_clave(parametro1);
-				break;
-			case deadlock:
-				deadlock_consola();
-				break;
-			case salir:
-				printf("Terminando consola...\n");
-				break;
-			default:
-				printf("No reconozco el comando vieja...\n");
-				break;
-		}
-
-		//Limpio el parametro 1
-		if(parametro1 != NULL)
-		{
-			free(parametro1);
-			parametro1 = NULL;
-		}
-
-		//Limpio el parametro 2
-		if(parametro2 != NULL)
-		{
-			free(parametro2);
-			parametro2 = NULL;
-		}
-
 		//Sale de la consola con exit
-		if(!strcmp(comando,"exit")){
-			free(comando);
+		if(res)
 			break;
-		}
-		else{
-			free(comando);
-		}
+
 	}
 
 	pthread_exit(0);
@@ -368,4 +395,192 @@ void deadlock_consola(void)
 	return;
 }
 
+int comando_consola(char * buffer){
 
+	int comando_key;
+	char *comando = NULL;
+	char *parametro1 = NULL;
+	char *parametro2 = NULL;
+	int res = 0;
+
+	//printf("string recibido: %s\n",buffer);
+
+	// Separa la linea de consola en comando y sus parametros
+	obtener_parametros(buffer, &comando, &parametro1, &parametro2);
+	//printf("comando: %s p1: %s p2: %s\n",comando,parametro1,parametro2);
+
+	// Obtiene la clave del comando a ejecutar para el switch
+	comando_key = obtener_key_comando(comando);
+
+	switch(comando_key){
+		case pausar:
+			pausar_consola();
+			break;
+		case continuar:
+			continuar_consola();
+			break;
+		case bloquear:
+			bloquear_clave(parametro1,parametro2);
+			break;
+		case desbloquear:
+			desbloquear_clave(parametro1, parametro2);
+			break;
+		case listar:
+			listar_recurso(parametro1);
+			break;
+		case kill:
+			kill_id(parametro1);
+			break;
+		case status:
+			status_clave(parametro1);
+			break;
+		case deadlock:
+			deadlock_consola();
+			break;
+		case salir:
+			res = 1;
+			printf("Saliendo de la consola\n");
+			break;
+		default:
+			printf("No reconozco el comando vieja...\n");
+			break;
+	}
+
+	//Limpio el parametro 1
+	if(parametro1 != NULL)
+	{
+		free(parametro1);
+		parametro1 = NULL;
+	}
+
+	//Limpio el parametro 2
+	if(parametro2 != NULL)
+	{
+		free(parametro2);
+		parametro2 = NULL;
+	}
+
+
+	free(comando);
+
+	return res;
+
+}
+
+int atender_nuevo_esi(int serv_socket)
+{
+	struct sockaddr_in client_addr;
+	memset(&client_addr, 0, sizeof(client_addr));
+	socklen_t client_len = sizeof(client_addr);
+
+	int new_client_sock = accept(serv_socket, (struct sockaddr *)&client_addr, &client_len);
+	if (new_client_sock < 0) {
+	  perror("accept()");
+	  return -1;
+	}
+
+	printf("Acepté al esi:%d.\n", new_client_sock);
+
+	int i;
+	for (i = 0; i < MAX_CLIENTES; ++i) {
+
+		if (conexiones_esi[i].socket == NO_SOCKET) {
+			conexiones_esi[i].socket = new_client_sock;
+			conexiones_esi[i].addres = client_addr;
+	        return new_client_sock;
+	    }
+
+	 }
+
+	 // printf("Demasiadas conexiones. Cerrando nueva conexion %s:%d.\n", client_ipv4_str, client_addr.sin_port);
+	 close(new_client_sock);
+
+	 return -1;
+
+}
+
+void inicializar_conexiones_esi(void)
+{
+	for (int i = 0; i < MAX_CLIENTES; ++i)
+	{
+		conexiones_esi[i].socket = NO_SOCKET;
+	}
+
+}
+
+int recibir_mensaje_esi(int esi_socket)
+{
+	int read_size;
+	char client_message[2000];
+
+	read_size = recv(esi_socket , client_message , 2000 , 0);
+	if(read_size > 0)
+	{
+		printf("Esi %d dice: %s\n",esi_socket,client_message);
+		int res_send = send(esi_socket, client_message, sizeof(client_message), 0);
+	}
+
+
+	return read_size;
+}
+
+int cerrar_conexion_esi(t_conexion_esi * esi)
+{
+
+	printf("Conexion con esi %d cerrada\n",esi->socket);
+	close(esi->socket);
+	esi->socket = NO_SOCKET;
+
+	return 0;
+}
+
+int read_from_stdin(char *read_buffer, size_t max_len)
+{
+	char c = '\0';
+	int i = 0;
+
+	memset(read_buffer, 0, max_len);
+
+	ssize_t read_count = 0;
+	ssize_t total_read = 0;
+
+	do{
+		read_count = read(STDIN_FILENO, &c, 1);
+		if (read_count < 0 && errno != EAGAIN && errno != EWOULDBLOCK) {
+		  perror("read()");
+		  return -1;
+		}
+		else if (read_count < 0 && (errno == EAGAIN || errno == EWOULDBLOCK)) {
+		  break;
+		}
+		else if (read_count > 0) {
+		  total_read += read_count;
+
+		  read_buffer[i] = c;
+		  i++;
+
+		  if (total_read > max_len) {
+			//printf("Message too large and will be chopped. Please try to be shorter next time.\n");
+			fflush(STDIN_FILENO);
+			break;
+		  }
+		}
+	}while ((read_count > 0) && (total_read < max_len) && (c != '\n'));
+
+	size_t len = strlen(read_buffer);
+	if (len > 0 && read_buffer[len - 1] == '\n')
+		read_buffer[len - 1] = '\0';
+
+	//printf("Read from stdin %zu bytes. Let's prepare message to send.\n", strlen(read_buffer));
+
+	return 0;
+}
+
+void stdin_no_bloqueante(void)
+{
+	  /* Set nonblock for stdin. */
+	  int flag = fcntl(STDIN_FILENO, F_GETFL, 0);
+	  flag |= O_NONBLOCK;
+	  fcntl(STDIN_FILENO, F_SETFL, flag);
+
+}
--- Planificador/src/Planificador.h
+++ Planificador/src/Planificador.h
@@ -13,27 +13,64 @@
 #include <stdlib.h>
 #include <string.h>			// funciones de strings
 #include <sys/socket.h>		// Para crear los sockets
+#include <sys/select.h>		// Select
 #include <sys/types.h>
+#include <sys/time.h>		//timeval en select
 #include <netdb.h> 			// Para getaddrinfo
 #include <unistd.h> 		// Para close(socket)
 #include <pthread.h>		// Manejo de hilos
 #include <readline/readline.h> // Para usar readline
 #include <readline/history.h> // Para usar readline
 #include <commons/string.h> // Para manejo de strings
+#include <errno.h>			//errorno
+#include <fcntl.h>			// std no block
 
 
-#define IP "127.0.0.1"
-#define PORT "8080"
+/**********************************************/
+/* DEFINES									  */
+/**********************************************/
+#define IP_COORD "127.0.0.1"
+#define PORT_COORD "8080"
+#define PORT_ESCUCHA 8080
+#define STDIN 0
+#define TRUE 1
+#define FALSE 0
+#define MAX_CLIENTES 20
+#define MAX_LINEA 255
+#define NO_SOCKET -1
 
-void *conectar_coordinador();
-void *consola();
-void *iniciar_servidor();
 
-int obtener_key_comando(char* comando);
-void obtener_parametros(char* buffer, char** comando, char** parametro1, char** parametro2);//Falta implementarla
+
+/**********************************************/
+/* STRUCTS									  */
+/**********************************************/
+typedef struct {
+  int socket;
+  struct sockaddr_in addres;
+
+} t_conexion_esi;
+
+/**********************************************/
+/* DATOS GLOBALES							  */
+/**********************************************/
+t_conexion_esi conexiones_esi[MAX_CLIENTES];
 
 //Enumeracion de los comandos de la consola
 enum comandos { pausar, continuar, bloquear, desbloquear, listar, kill, status, deadlock, salir };
+
+
+/**********************************************/
+/* FUNCIONES								  */
+/**********************************************/
+int conectar_coordinador(char * ip, char * port);
+void *consola();
+int iniciar_servidor(unsigned short port);
+int comando_consola(char * buffer);
+void stdin_no_bloqueante(void);
+
+int obtener_key_comando(char* comando);
+void obtener_parametros(char* buffer, char** comando, char** parametro1, char** parametro2);//Falta implementarla
+int read_from_stdin(char *read_buffer, size_t max_len);
 
 void pausar_consola(void);
 void continuar_consola(void);
@@ -43,3 +80,8 @@ void listar_recurso(char* recurso);
 void kill_id(char* id);
 void status_clave(char* clave);
 void deadlock_consola(void);
+
+void inicializar_conexiones_esi(void);
+int atender_nuevo_esi(int serv_socket);
+int recibir_mensaje_esi(int esi_socket);
+int cerrar_conexion_esi(t_conexion_esi * esi);
