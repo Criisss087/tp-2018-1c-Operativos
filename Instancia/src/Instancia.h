795455c45398790c693cf2a91e5e67f6f89589ad
Sebastian Morales <moralessebastian46@gmail.com>
2018-06-07 00:00:30 -0300

    Implementa algoritmo circular + creacion de FuncionesInstancia.c

--- Instancia/src/Instancia.h
+++ Instancia/src/Instancia.h
@@ -15,13 +15,14 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <commons/collections/list.h> // Para manejo de strings
+#include <sys/mman.h> // Para el uso de mmap()
 
-#define NOMBRE_INSTANCIA "Instancia1"
 #define IP_COORDINADOR "127.0.0.1"
 #define PUERTO_COORDINADOR 8888
 #define PACKAGESIZE 1024
+#define PUNTO_DE_MONTAJE "home/utnso/instancia1/"
+#define NOMBRE_INSTANCIA "Instancia1"
 #define INTERVALO_DUMP 10 // Intervalo dado en segundos para guardar la tabla de entradas en archivo de texto plano
-#define PUNTO_DE_MONTAJE "home/utnso/instanciaX"
 
 //***Cod ops
 #define INSTANCIA_COORDINADOR_CONEXION 1
@@ -29,6 +30,7 @@
 #define COORDINADOR_INSTANCIA_SENTENCIA 3
 #define INSTANCIA_COORDINADOR_RESPUESTA_SENTENCIA 4
 
+enum algoritmos { circ, lru, bsu };
 
 // struct para el envio de nombre de Instancia al Coordinador
 typedef struct{
@@ -38,8 +40,8 @@ typedef struct{
 typedef struct{
 		int keyword;
 		char clave[40];
-		char * valor;
-	} t_sentencia;
+		char* valor;
+	} __attribute__((packed)) t_sentencia;
 
 t_configTablaEntradas * configTablaEntradas;
 
@@ -54,13 +56,11 @@ typedef struct{
 	int tamanioValor;
 	bool esAtomica;
 	char* puntero;
+	char* punteroArchivo;
 } __attribute__((packed)) t_indice_entrada;
 
 int numeroEntrada = 0;
 
 char * tablaEntradas;
-
-// Constante auxiliar para la busqueda de una clave en la tabla de indices
-char claveBuscada[40];
 
 #endif /* SRC_UTILIDADES_H_ */
